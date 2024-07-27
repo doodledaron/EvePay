139e1f9053dc26ed439c2e6fdac99665330f9028
Alpha Chong <alphachongs@gmail.com>
2024-07-27 20:33:13 +0800

    updated connection django with next.js

--- backend-django/api_token/views.py
+++ backend-django/api_token/views.py
@@ -18,8 +18,8 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 # Token API_KEY and API_PASSWORD
-API_KEY = ''
-API_PASSWORD = ''
+API_KEY = 'd2e5b49b8d126cd2687d9c61b8f6ba2e64e856d6c34cdf27fb3cb9b59bd2d2a3'
+API_PASSWORD = 'sk_43375ea6665657b0030e702f30144c664239d43c03f0c9b733afd41d47db02aa'
 BASE_API_URL = 'https://service-testnet.maschain.com/api/token'
 
 # Headers Credentials For Maschain API
@@ -46,114 +46,6 @@ def test_api_token_conn(request):
 @api_view(['POST'])
 def mint_token(request):
     API_URL = f'{BASE_API_URL}/mint'
-
-    try:
-        # Get data from the request body
-        data = request.data
-        logger.debug(f"Request Data: {data}")
-        
-        # Make the POST request to the external API with the data
-        response = requests.post(API_URL, headers=headers, json=data)
-        response.raise_for_status()  # Raises an HTTPError for bad responses (4xx and 5xx)
-        
-        if response.status_code == 200:
-            logger.debug(f"Response Data: {response.json()}")
-            return JsonResponse({'status': 'success', 'data': response.json()})
-        else:
-            return JsonResponse({'status': 'error', 'message': response.text}, status=response.status_code)
-    
-    except requests.exceptions.HTTPError as http_err:
-        logger.error(f"HTTP error occurred: {http_err} - {response.status_code} - {response.text}")
-        return JsonResponse({'status': 'error', 'message': f'HTTP error occurred: {http_err}'}, status=500)
-    except requests.exceptions.ConnectionError as conn_err:
-        logger.error(f"Connection error occurred: {conn_err}")
-        return JsonResponse({'status': 'error', 'message': f'Connection error occurred: {conn_err}'}, status=500)
-    except requests.exceptions.Timeout as timeout_err:
-        logger.error(f"Timeout error occurred: {timeout_err}")
-        return JsonResponse({'status': 'error', 'message': f'Timeout error occurred: {timeout_err}'}, status=500)
-    except requests.exceptions.RequestException as req_err:
-        logger.error(f"An error occurred: {req_err}")
-        return JsonResponse({'status': 'error', 'message': f'An error occurred: {req_err}'}, status=500)
-    except Exception as e:
-        logger.error(f"Unexpected error occurred: {e}")
-        return JsonResponse({'status': 'error', 'message': f'Unexpected error occurred: {e}'}, status=500)
-
-# Pause Token
-@api_view(['POST'])
-def pause_token(request):
-    API_URL = f'{BASE_API_URL}/pause'
-
-    try:
-        # Get data from the request body
-        data = request.data
-        logger.debug(f"Request Data: {data}")
-        
-        # Make the POST request to the external API with the data
-        response = requests.post(API_URL, headers=headers, json=data)
-        response.raise_for_status()  # Raises an HTTPError for bad responses (4xx and 5xx)
-        
-        if response.status_code == 200:
-            logger.debug(f"Response Data: {response.json()}")
-            return JsonResponse({'status': 'success', 'data': response.json()})
-        else:
-            return JsonResponse({'status': 'error', 'message': response.text}, status=response.status_code)
-    
-    except requests.exceptions.HTTPError as http_err:
-        logger.error(f"HTTP error occurred: {http_err} - {response.status_code} - {response.text}")
-        return JsonResponse({'status': 'error', 'message': f'HTTP error occurred: {http_err}'}, status=500)
-    except requests.exceptions.ConnectionError as conn_err:
-        logger.error(f"Connection error occurred: {conn_err}")
-        return JsonResponse({'status': 'error', 'message': f'Connection error occurred: {conn_err}'}, status=500)
-    except requests.exceptions.Timeout as timeout_err:
-        logger.error(f"Timeout error occurred: {timeout_err}")
-        return JsonResponse({'status': 'error', 'message': f'Timeout error occurred: {timeout_err}'}, status=500)
-    except requests.exceptions.RequestException as req_err:
-        logger.error(f"An error occurred: {req_err}")
-        return JsonResponse({'status': 'error', 'message': f'An error occurred: {req_err}'}, status=500)
-    except Exception as e:
-        logger.error(f"Unexpected error occurred: {e}")
-        return JsonResponse({'status': 'error', 'message': f'Unexpected error occurred: {e}'}, status=500)
-
-# Resume Token
-@api_view(['POST'])
-def resume_token(request):
-    API_URL = f'{BASE_API_URL}/resume'
-
-    try:
-        # Get data from the request body
-        data = request.data
-        logger.debug(f"Request Data: {data}")
-        
-        # Make the POST request to the external API with the data
-        response = requests.post(API_URL, headers=headers, json=data)
-        response.raise_for_status()  # Raises an HTTPError for bad responses (4xx and 5xx)
-        
-        if response.status_code == 200:
-            logger.debug(f"Response Data: {response.json()}")
-            return JsonResponse({'status': 'success', 'data': response.json()})
-        else:
-            return JsonResponse({'status': 'error', 'message': response.text}, status=response.status_code)
-    
-    except requests.exceptions.HTTPError as http_err:
-        logger.error(f"HTTP error occurred: {http_err} - {response.status_code} - {response.text}")
-        return JsonResponse({'status': 'error', 'message': f'HTTP error occurred: {http_err}'}, status=500)
-    except requests.exceptions.ConnectionError as conn_err:
-        logger.error(f"Connection error occurred: {conn_err}")
-        return JsonResponse({'status': 'error', 'message': f'Connection error occurred: {conn_err}'}, status=500)
-    except requests.exceptions.Timeout as timeout_err:
-        logger.error(f"Timeout error occurred: {timeout_err}")
-        return JsonResponse({'status': 'error', 'message': f'Timeout error occurred: {timeout_err}'}, status=500)
-    except requests.exceptions.RequestException as req_err:
-        logger.error(f"An error occurred: {req_err}")
-        return JsonResponse({'status': 'error', 'message': f'An error occurred: {req_err}'}, status=500)
-    except Exception as e:
-        logger.error(f"Unexpected error occurred: {e}")
-        return JsonResponse({'status': 'error', 'message': f'Unexpected error occurred: {e}'}, status=500)
-
-# Burn Token
-@api_view(['POST'])
-def burn_token(request):
-    API_URL = f'{BASE_API_URL}/burn'
 
     try:
         # Get data from the request body
@@ -320,6 +212,42 @@ def check_balance(address):
         logger.error(f"Unexpected error occurred: {e}")
         return JsonResponse({'status': 'error', 'message': f'Unexpected error occurred: {e}'}, status=500)
 
+@api_view(['POST'])
+def check_balance_api(request):
+    API_URL = f'{BASE_API_URL}/balance'
+    contract_address = '0xA10b5960afae880bA86cb3Bb5ec1Ae2eBAe19083'
+
+    try:
+        data = request.data
+        logger.debug(f"Request Data: {data}")
+        
+        # Make the POST request to the external API with the data
+        response = requests.post(API_URL, headers=headers, json=data)
+        response.raise_for_status()  # Raises an HTTPError for bad responses (4xx and 5xx)
+        
+        if response.status_code == 200:
+            response_data = response.json()
+            logger.info(f"API Response Data: {response_data}")
+            return JsonResponse({'status': 'success', 'data': response_data})
+        else:
+            return JsonResponse({'status': 'error', 'message': response.text}, status=response.status_code)
+
+    except requests.exceptions.HTTPError as http_err:
+        logger.error(f"HTTP error occurred: {http_err} - {response.status_code} - {response.text}")
+        return JsonResponse({'status': 'error', 'message': f'HTTP error occurred: {http_err}'}, status=500)
+    except requests.exceptions.ConnectionError as conn_err:
+        logger.error(f"Connection error occurred: {conn_err}")
+        return JsonResponse({'status': 'error', 'message': f'Connection error occurred: {conn_err}'}, status=500)
+    except requests.exceptions.Timeout as timeout_err:
+        logger.error(f"Timeout error occurred: {timeout_err}")
+        return JsonResponse({'status': 'error', 'message': f'Timeout error occurred: {timeout_err}'}, status=500)
+    except requests.exceptions.RequestException as req_err:
+        logger.error(f"An error occurred: {req_err}")
+        return JsonResponse({'status': 'error', 'message': f'An error occurred: {req_err}'}, status=500)
+    except Exception as e:
+        logger.error(f"Unexpected error occurred: {e}")
+        return JsonResponse({'status': 'error', 'message': f'Unexpected error occurred: {e}'}, status=500)
+
 # Get Transaction Filter To
 @api_view(['GET'])
 def get_transaction_filter_to(request, wallet_address):
@@ -429,6 +357,77 @@ def get_transaction_filter_from(request, wallet_address):
         else:
             return JsonResponse({'status': 'error', 'message': response.text}, status=response.status_code)
     
+    except requests.exceptions.HTTPError as http_err:
+        logger.error(f"HTTP error occurred: {http_err} - {response.status_code} - {response.text}")
+        return JsonResponse({'status': 'error', 'message': f'HTTP error occurred: {http_err}'}, status=500)
+    except requests.exceptions.ConnectionError as conn_err:
+        logger.error(f"Connection error occurred: {conn_err}")
+        return JsonResponse({'status': 'error', 'message': f'Connection error occurred: {conn_err}'}, status=500)
+    except requests.exceptions.Timeout as timeout_err:
+        logger.error(f"Timeout error occurred: {timeout_err}")
+        return JsonResponse({'status': 'error', 'message': f'Timeout error occurred: {timeout_err}'}, status=500)
+    except requests.exceptions.RequestException as req_err:
+        logger.error(f"An error occurred: {req_err}")
+        return JsonResponse({'status': 'error', 'message': f'An error occurred: {req_err}'}, status=500)
+    except Exception as e:
+        logger.error(f"Unexpected error occurred: {e}")
+        return JsonResponse({'status': 'error', 'message': f'Unexpected error occurred: {e}'}, status=500)
+
+@api_view(['GET'])
+def get_combined_transactions(request, wallet_address):
+    """
+    Fetch and combine transactions from the external API with both 'to' and 'from' filters.
+    
+    Args:
+        wallet_address (str): The wallet address to filter transactions.
+    
+    Returns:
+        JsonResponse: A JSON response with the status and combined data or error message.
+    """
+
+    API_URL = f'{BASE_API_URL}/get-token-transaction'
+    contract_address = '0xA10b5960afae880bA86cb3Bb5ec1Ae2eBAe19083'
+    filters = ['to', 'from']
+    combined_data = []
+
+    try:
+        for filter_type in filters:
+            params = {
+                'wallet_address': wallet_address,
+                'contract_address': contract_address,
+                'filter': filter_type
+            }
+
+            # Make the GET request to the external API
+            response = requests.get(API_URL, headers=headers, params=params)
+            response.raise_for_status()  # Raises an HTTPError for bad responses (4xx and 5xx)
+
+            if response.status_code == 200:
+                data = response.json()
+                transactions = data.get('result', [])
+                for transaction in transactions:
+                    from_address = transaction.get('from')
+                    to_address = transaction.get('to')
+                    timestamp = transaction.get('timestamp')
+                    amount = transaction.get('amount')
+
+                    if from_address:
+                        from_details = get_wallet_adrr(from_address)
+                        transaction['from'] = from_details.get('result', {}).get('name', 'Unknown')
+                    if to_address:
+                        to_details = get_wallet_adrr(to_address)
+                        transaction['to'] = to_details.get('result', {}).get('name', 'Unknown')
+                    if timestamp:
+                        transaction['timestamp'] = convert_to_readable_timestamp(timestamp)
+                    if amount:
+                        transaction['amount'] = f'{float(amount) / 100:.2f} EVEC'
+
+                combined_data.extend(transactions)
+            else:
+                return JsonResponse({'status': 'error', 'message': response.text}, status=response.status_code)
+
+        return JsonResponse({'status': 'success', 'data': combined_data})
+
     except requests.exceptions.HTTPError as http_err:
         logger.error(f"HTTP error occurred: {http_err} - {response.status_code} - {response.text}")
         return JsonResponse({'status': 'error', 'message': f'HTTP error occurred: {http_err}'}, status=500)
