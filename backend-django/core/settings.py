24b78d82eb6c446b328ceac7649c460f13e81c92
Tang Yu Xuan <67047462+doodledaron@users.noreply.github.com>
2024-07-28 09:43:59 +0800

    Update settings.py

--- backend-django/core/settings.py
+++ backend-django/core/settings.py
@@ -65,9 +65,7 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000',  
-]
+CORS_ALLOWED_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'core.urls'
 
